7014cda7156587516f36bae3f408d003fe7bc536
Harrison Kussie <harrison.kussie@saberastro.com>
2026-03-24 19:17:45 -0700

    Fix next.config and add vercel.json for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['xlsx'],
+  experimental: {
+    serverComponentsExternalPackages: ['xlsx'],
+  },
 }
 
 module.exports = nextConfig
